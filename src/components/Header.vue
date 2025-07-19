fe043f6388a67da01307bb18dd450c8703eaed9e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-07-18 20:23:17 -0600

    feat: :sparkles: Initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>This is a title font test</h1>
+    <p>This is a paragraph font test</p>
+    <code>This is a code font test console.log()</code>
+</template>
+
+<style scoped></style>
